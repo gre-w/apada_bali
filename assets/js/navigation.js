f4d02d7aabce894351f0a998957f901b368c2db0
gre-w <gr8orius@gmail.com>
2023-07-28 16:07:08 +0800

    small fix (change 2)

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,8 +1,14 @@
 const openPage1Button = document.getElementById('openPort');
+const openPage2Button = document.getElementById('openPort1');
 
 
 openPage1Button.addEventListener('click', function() {
   // Open page1.html in a new tab
-  window.open('../pages/services.html', '_blank');
+  window.open('../pages/services.html');
+});
+
+openPage2Button.addEventListener('click', function() {
+  // Open page1.html in a new tab
+  window.open('.../pages/services.html');
 });
 

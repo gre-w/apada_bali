2b0f5a8defe1d52c16702d93d077b959f446209b
gre-w <12gaugebox@gmail.com>
2024-03-30 20:51:37 +0800

    2024 update

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,14 +1,13 @@
 const openPage1Button = document.getElementById('openPort');
 const openPage2Button = document.getElementById('openPort1');
-
+const openPage3Button = document.getElementById('openPort2');
 
 openPage1Button.addEventListener('click', function() {
-  // Open page1.html in a new tab
-  window.open('./pages/services.html');
+  window.open('./pages/portofolio.html');
 });
-
 openPage2Button.addEventListener('click', function() {
-  // Open page1.html in a new tab
-  window.open('./pages/services.html');
+  window.open('./pages/portofolio2.html');
 });
-
+openPage3Button.addEventListener('click', function() {
+  window.open('./pages/afb.html');
+});

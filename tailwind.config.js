308c347cb2d62be40b00111cd7c57bb9ec4a7874
glwf <yudiantaragregorius+github@gmail.com>
2024-08-09 17:12:46 +0800

    small tweak & add intern section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "background.js", "./pages/*.html", "./assets/css/*.css", "./assets/js/*.js"],
+  content: ["index.html", "./pages/*.html", "./assets/css/*.css", "./assets/js/*.js"],
   theme: {
     container: {
       center: true,

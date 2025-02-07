ace5f38a9d059806595d4157d18dd8b3cb897ea0
nosaphed <yudiantaragregorius+github@gmail.com>
2025-02-07 22:11:41 +0800

    tuck up some layout, naming covention, and adding pics

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,11 +3,11 @@ const openPage2Button = document.getElementById('openPort1');
 const openPage3Button = document.getElementById('openPort2');
 
 openPage1Button.addEventListener('click', function() {
-  window.open('./pages/portofolio.html');
+  window.open('./pages/portofolio-asb.html');
 });
 openPage2Button.addEventListener('click', function() {
-  window.open('./pages/portofolio2.html');
+  window.open('./pages/portofolio-ajb.html');
 });
 openPage3Button.addEventListener('click', function() {
-  window.open('./pages/afb.html');
+  window.open('./pages/portofolio-afb.html');
 });
